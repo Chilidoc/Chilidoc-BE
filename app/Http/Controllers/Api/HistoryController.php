dfbd9f6d15200e864eb1c76753de05201fc8f0bd
IlhamFiqri <ilhamfiqri12345@gmail.com>
2024-06-06 14:03:05 +0700

    Setup deploy

--- app/Http/Controllers/Api/HistoryController.php
+++ app/Http/Controllers/Api/HistoryController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use App\Helper\ResponseHelper;
+use App\Models\History;
+use \Auth;
 
 class HistoryController extends Controller
 {
